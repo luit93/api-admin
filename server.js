3ec0b659d8d8272a1448039e0c6171f5f81d1b72
luit <luit93@gmail.com>
2021-09-15 09:13:16 +1000

    server ready

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("h3llo");
+import express from 'express'
+const app = express()
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+const PORT = process.env.PORT || 8000
+//middleware
+app.use(helmet())
+app.use(cors())
+app.use(morgan())
+app.use('/', (req, res) => {
+  res.send('You have reached the end of the router list')
+})
+
+app.listen(PORT, (error) => {
+  if (error) {
+    return console.log(error)
+  }
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
